23e563888c17f637488a6d7adbf0d476625a0664
Niels Marquardt <niels.marquardt@gmail.com>
2025-01-06 14:56:10 +0100

    updated hamburger button

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    document.getElementById('nav-menu').classList.toggle('open');
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const navMenu = document.getElementById('nav-menu');
+    const navLinks = document.querySelectorAll('#nav-menu a');
+
+    hamburgerMenu.addEventListener('click', function() {
+        navMenu.classList.toggle('open');
+        hamburgerMenu.classList.toggle('open'); // Toggle class on hamburger menu
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navMenu.classList.remove('open');
+            hamburgerMenu.classList.remove('open'); // Remove class from hamburger menu
+        });
+    });
 });
